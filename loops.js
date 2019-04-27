90fd24e80659f4a75263736fda4d47388ef5af9b
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 22:58:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,5 +18,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
-  
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < num);
 }
